1e9461303566918c885861005399613a9bd74670
Jane He <jane.he@datastax.com>
2025-01-16 17:38:24 +0800

    CASSNODEJS-8: First TypeScript PR: Massive rename patch by Jane He; reviewed by Jorge Bay

--- lib/writers.ts
+++ lib/writers.ts
@@ -19,7 +19,7 @@
 const events = require('events');
 
 const types = require('./types');
-const utils = require('./utils.js');
+const utils = require('./utils');
 const FrameHeader = types.FrameHeader;
 
 /**
